8c3304bc3632d3271401b6b2f213a01269e04ca3
Alex <keeper.gen@gmil.com>
2022-04-14 18:49:36 +0300

    feat: pagination funcion + propTypes + eslint

--- src/components/bookmark.jsx
+++ src/components/bookmark.jsx
@@ -1,11 +1,16 @@
 import React from "react";
+import PropTypes from "prop-types";
 
-const BookMark = ({ status, ...rest }) => {
-	return (
-		<button className="btn btn-outline-info" {...rest}>
-			<i className={"bi bi-bookmark" + (status ? "-star-fill" : "")}></i>
-		</button>
-	);
+const Bookmark = ({ status, ...rest }) => {
+    return (
+        <button className="btn btn-outline-info" {...rest}>
+            <i className={"bi bi-bookmark" + (status ? "-star-fill" : "")}></i>
+        </button>
+    );
 };
 
-export default BookMark;
+Bookmark.propTypes = {
+    status: PropTypes.bool.isRequired
+};
+
+export default Bookmark;
--- src/components/quality.jsx
+++ src/components/quality.jsx
@@ -1,9 +1,13 @@
 import React from "react";
+import PropTypes from "prop-types";
 
-const Quality = ({color, name} ) => {
-	return (
-		<span className={`badge bg-${color} m-2`}>{name}</span>
-	)
+const Quality = ({ color, name }) => {
+    return <span className={`badge bg-${color} m-2`}>{name}</span>;
 };
 
-export default Quality
+Quality.propTypes = {
+    color: PropTypes.string.isRequired,
+    name: PropTypes.string.isRequired
+};
+
+export default Quality;
