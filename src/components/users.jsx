54a7b8cbda6a0d612211de51db1a2bc9d2d1bf87
Alex <keeper.gen@gmil.com>
2022-05-23 16:01:11 +0300

    refactor: Made universal sorting function

--- src/components/users.jsx
+++ src/components/users.jsx
@@ -32,14 +32,7 @@ const Users = ({ users: allUsers, ...rest }) => {
     };
 
     const handleSort = (item) => {
-        if (sortBy.iter === item) {
-            setSortBy((prevState) => ({
-                ...prevState,
-                order: prevState.order === "asc" ? "desc" : "asc"
-            }));
-        } else {
-            setSortBy({ iter: item, order: "asc" });
-        }
+        setSortBy(item);
     };
 
     const filteredUsers = selectedProf ? allUsers.filter((user) => JSON.stringify(user.profession) === JSON.stringify(selectedProf)) : allUsers;
@@ -61,7 +54,7 @@ const Users = ({ users: allUsers, ...rest }) => {
             )}
             <div className="d-flex flex-column p-2">
                 <SearchStatus length={count} />
-                {count > 0 && <UsersTable users={userCrop} onSort={handleSort} {...rest}/>}
+                {count > 0 && <UsersTable users={userCrop} onSort={handleSort} currentSort={sortBy} {...rest}/>}
                 <div className="d-flex justify-content-center">
                     <Pagination
                         itemsCount={count}
--- src/components/usersTable.jsx
+++ src/components/usersTable.jsx
@@ -2,30 +2,44 @@ import React from "react";
 import User from "./user";
 import PropTypes from "prop-types";
 
-const UsersTable = ({ users, onSort, ...rest }) => {
-    return <table className="table table-hover">
-        <thead>
-            <tr className="table-secondary">
-                <th onClick={() => onSort("name")} scope="col">Имя</th>
-                <th scope="col">Качества</th>
-                <th onClick={() => onSort("profession.name")} scope="col">Профессия</th>
-                <th onClick={() => onSort("completedMeetings")} scope="col">Встретился, раз</th>
-                <th onClick={() => onSort("rate")} scope="col">Оценка</th>
-                <th onClick={() => onSort("bookmark")} scope="col">Избранное</th>
-                <th scope="col"></th>
-            </tr>
-        </thead>
-        <tbody>
-            {users.map((user) => (
-                <User key={user._id} {...rest} {...user} />
-            ))}
-        </tbody>
-    </table>;
+const UsersTable = ({ users, onSort, currentSort, ...rest }) => {
+    const handleSort = (item) => {
+        if (currentSort.iter === item) {
+            onSort({
+                ...currentSort,
+                order: currentSort.order === "asc" ? "desc" : "asc"
+            });
+        } else {
+            onSort({ iter: item, order: "asc" });
+        }
+    };
+
+    return (
+        <table className="table table-hover">
+            <thead>
+                <tr className="table-secondary">
+                    <th onClick={() => handleSort("name")} scope="col">Имя</th>
+                    <th scope="col">Качества</th>
+                    <th onClick={() => handleSort("profession.name")} scope="col">Профессия</th>
+                    <th onClick={() => handleSort("completedMeetings")} scope="col">Встретился, раз</th>
+                    <th onClick={() => handleSort("rate")} scope="col">Оценка</th>
+                    <th onClick={() => handleSort("bookmark")} scope="col">Избранное</th>
+                    <th scope="col"></th>
+                </tr>
+            </thead>
+            <tbody>
+                {users.map((user) => (
+                    <User key={user._id} {...rest} {...user} />
+                ))}
+            </tbody>
+        </table>
+    );
 };
 
 UsersTable.propTypes = {
     users: PropTypes.array.isRequired,
-    onSort: PropTypes.func.isRequired
+    onSort: PropTypes.func.isRequired,
+    currentSort: PropTypes.object.isRequired
 };
 
 export default UsersTable;
