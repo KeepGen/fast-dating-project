f07a2f0e43ec0e499e8311ee9c2ff79ba1e29588
Alex <keeper.gen@gmil.com>
2022-06-01 18:29:26 +0300

    refactor: Change the code according to lector's code

--- src/components/usersTable.jsx
+++ src/components/usersTable.jsx
@@ -5,14 +5,14 @@ import TableBody from "./tableBody";
 import Bookmark from "./bookmark";
 import QualitiesList from "./qualitiesList";
 import Table from "./table";
-import UsersList from "./usersList";
+import { Link } from "react-router-dom";
 
 const UsersTable = ({ users, onSort, selectedSort, onToggleBookMark, onDelete }) => {
     const columns = {
         name: {
             path: "name",
             name: "Имя",
-            component: (user) => <UsersList users={user} />
+            component: (user) => <Link to={`/users/${user._id}`}>{user.name}</Link>
         },
         qualities: { name: "Качества", component: (user) => (<QualitiesList qualities={user.qualities} />) },
         professions: { path: "profession.name", name: "Профессия" },
