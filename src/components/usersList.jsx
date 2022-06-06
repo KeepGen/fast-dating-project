ada6be56360bf7532cf3f06bed091bf3b4a9460d
Alex <keeper.gen@gmil.com>
2022-06-06 17:36:38 +0300

    feat: Added Search function

--- src/components/usersList.jsx
+++ src/components/usersList.jsx
@@ -11,6 +11,7 @@ import _ from "lodash";
 const UsersList = () => {
     const [currentPage, setCurrentPage] = useState(1);
     const [professions, setProfessions] = useState();
+    const [searchQuery, setSearchQuery] = useState("");
     const [selectedProf, setSelectedProf] = useState();
     const [sortBy, setSortBy] = useState({ path: "name", order: "asc" });
     const pageSize = 8;
@@ -42,10 +43,16 @@ const UsersList = () => {
 
     useEffect(() => {
         setCurrentPage(1);
-    }, [selectedProf]);
+    }, [selectedProf, searchQuery]);
 
     const handleProfessionSelect = (item) => {
+        if (searchQuery !== "") setSearchQuery("");
         setSelectedProf(item);
+    };
+
+    const handleSearchQuery = ({ target }) => {
+        setSelectedProf(undefined);
+        setSearchQuery(target.value);
     };
 
     const handlePageChange = (pageIndex) => {
@@ -56,12 +63,14 @@ const UsersList = () => {
         setSortBy(item);
     };
     if (users) {
-        const filteredUsers = selectedProf
-            ? users.filter(
-                (user) =>
-                    JSON.stringify(user.profession) === JSON.stringify(selectedProf)
-            )
-            : users;
+        const filteredUsers = searchQuery
+            ? users.filter((user) => user.name.toLowerCase().indexOf(searchQuery.toLowerCase()) !== -1)
+            : selectedProf
+                ? users.filter(
+                    (user) =>
+                        JSON.stringify(user.profession) === JSON.stringify(selectedProf)
+                )
+                : users;
 
         const count = filteredUsers.length;
         const sortedUsers = _.orderBy(filteredUsers, sortBy.path, sortBy.order);
@@ -81,6 +90,13 @@ const UsersList = () => {
                 )}
                 <div className="d-flex flex-column p-2">
                     <SearchStatus length={count} />
+                    <input
+                        type="text"
+                        name="searchQuery"
+                        placeholder="Search..."
+                        onChange={handleSearchQuery}
+                        value={searchQuery}
+                    />
                     <>
                         {count > 0 && (
                             <UsersTable
