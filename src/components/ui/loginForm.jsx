46864a289442a3161f3ba71efdeb7d9418025b27
Alex <keeper.gen@gmil.com>
2022-06-08 14:49:50 +0300

    feat: Added CheckBox, SelectBox, RadioBox sections on Register Form

--- src/components/ui/loginForm.jsx
+++ src/components/ui/loginForm.jsx
@@ -1,11 +1,12 @@
 import React, { useEffect, useState } from "react";
 import { validator } from "../../utils/validator";
 import TextField from "../form/textField";
+import CheckBoxField from "../form/checkBoxField";
 
 const LoginForm = () => {
-    const [data, setData] = useState({ email: "", password: "" });
+    const [data, setData] = useState({ email: "", password: "", stayOn: false });
     const [errors, setErrors] = useState({});
-    const handleChange = ({ target }) => {
+    const handleChange = (target) => {
         setData((prevState) => ({
             ...prevState,
             [target.name]: target.value
@@ -42,18 +43,12 @@ const LoginForm = () => {
         console.log(data);
     };
     return (
-        <div className="container mt-5">
-            <div className="row">
-                <div className="col-md-6 offset-md-3 shadow p-4">
-                    <h3 className="mb-4">Login Form</h3>
-                    <form onSubmit={handleSubmit}>
-                        <TextField label="Email" name="email" value={data.email} onChange={handleChange} error={errors.email} />
-                        <TextField label="Password" type="password" name="password" value={data.password} onChange={handleChange} error={errors.password} />
-                        <button type="submit" disabled={!isValid} className="btn btn-primary w-100 mx-auto">Submit</button>
-                    </form>
-                </div>
-            </div>
-        </div>
+        <form onSubmit={handleSubmit}>
+            <TextField label="Email" name="email" value={data.email} onChange={handleChange} error={errors.email} />
+            <TextField label="Password" type="password" name="password" value={data.password} onChange={handleChange} error={errors.password} />
+            <CheckBoxField value={data.stayOn} onChange={handleChange} name="stayOn" >Stay online</CheckBoxField>
+            <button type="submit" disabled={!isValid} className="btn btn-primary w-100 mx-auto">Submit</button>
+        </form>
     );
 };
 
